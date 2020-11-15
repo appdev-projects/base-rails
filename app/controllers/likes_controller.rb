a6837945de1be3f12c8ea7790615ce97fb3ab519
wfswee <wfswee@chicagobooth.edu>
2020-11-15 06:15:16 +0000

    Generate Likes table

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,56 @@
+class LikesController < ApplicationController
+  def index
+    matching_likes = Like.all
+
+    @list_of_likes = matching_likes.order({ :created_at => :desc })
+
+    render({ :template => "likes/index.html.erb" })
+  end
+
+  def show
+    the_id = params.fetch("path_id")
+
+    matching_likes = Like.where({ :id => the_id })
+
+    @the_like = matching_likes.at(0)
+
+    render({ :template => "likes/show.html.erb" })
+  end
+
+  def create
+    the_like = Like.new
+    the_like.course_id = params.fetch("query_course_id")
+    the_like.user_id = params.fetch("query_user_id")
+
+    if the_like.valid?
+      the_like.save
+      redirect_to("/likes", { :notice => "Like created successfully." })
+    else
+      redirect_to("/likes", { :notice => "Like failed to create successfully." })
+    end
+  end
+
+  def update
+    the_id = params.fetch("path_id")
+    the_like = Like.where({ :id => the_id }).at(0)
+
+    the_like.course_id = params.fetch("query_course_id")
+    the_like.user_id = params.fetch("query_user_id")
+
+    if the_like.valid?
+      the_like.save
+      redirect_to("/likes/#{the_like.id}", { :notice => "Like updated successfully."} )
+    else
+      redirect_to("/likes/#{the_like.id}", { :alert => "Like failed to update successfully." })
+    end
+  end
+
+  def destroy
+    the_id = params.fetch("path_id")
+    the_like = Like.where({ :id => the_id }).at(0)
+
+    the_like.destroy
+
+    redirect_to("/likes", { :notice => "Like deleted successfully."} )
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,25 @@ Rails.application.routes.draw do
 
 
 
+  # Routes for the Like resource:
+
+  # CREATE
+  post("/insert_like", { :controller => "likes", :action => "create" })
+          
+  # READ
+  get("/likes", { :controller => "likes", :action => "index" })
+  
+  get("/likes/:path_id", { :controller => "likes", :action => "show" })
+  
+  # UPDATE
+  
+  post("/modify_like/:path_id", { :controller => "likes", :action => "update" })
+  
+  # DELETE
+  get("/delete_like/:path_id", { :controller => "likes", :action => "destroy" })
+
+  #------------------------------
+
   # Routes for the Friend resource:
 
   # CREATE
