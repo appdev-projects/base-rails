331d299be58ca0af3bf2e1d04d74e70373a9652d
wfswee <wfswee@chicagobooth.edu>
2020-11-21 17:38:10 +0000

    results code minimized

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -5,33 +5,40 @@ class ResultsController < ApplicationController
     #  1. Who are the current user's friends?
     #  2. What courses do those friends like? I can't write an associate to get this because those tables aren't linked.
     #  3. For each course the current user has liked, count the number of their friends who have liked each of those courses. Be careful not to include the current user when calculating the number of likes.
-    #. 4. Store the course names and number of likes into an object, then print the results into a table.
+    #  4. Store the course names and number of likes into an object, then print the results into a table.
 
-    # LET'S BEGIN
-
-    # 1. FIND THE CURRENT USER'S FRIENDS
-
-    # (a) Create a blank array and push any friend record into it where the current user's id is the sender id.
-    users_friends = Array.new
+    # (a) Create a blank array and push all the IDs of the current user's friends into it.
+    recipient_ids = Array.new
 
     all_friends = Friend.all
     @list_of_friends = all_friends
 
     @list_of_friends.each do |a_friend|
       if @current_user.id == a_friend.sender_id
-        users_friends.push(a_friend)
+        recipient_ids.push(a_friend.recipient_id)
       end
     end
 
-    # (b) Extract the recipient id from each of those friend records and push them into a new array.
+    # (b) For each friend, pluck out the IDs of all the courses they have liked and push it into a new array.
+    
+    @matching_course_ids = Array.new
+      recipient_ids.each do |a_recipient|
+        @matching_course_ids << User.where({ :id => a_recipient }).at(0).likes.pluck(:course_id)
+      end
+    
+    # (c) Flatten the array and store in a hash key value pairs where key = course id and value = number of friends who like that course.
 
-    recipient_ids = Array.new
+    @matching_hash = @matching_course_ids.flatten.group_by{|e| e}.map{|k, v| [k, v.length]}.to_h
 
-    users_friends.each do |a_friend|
-      recipient_ids.push(a_friend.recipient_id)
-    end
+    # (d) Render the page that will show the resultst.
 
-    # (c) 
+    render({ :template => "results/index.html.erb" })
+
+  end
+
+  def unused_code
+    
+    # (b) For each of those friends, push into an array an array of the courses that they have liked. Then iterate through those sub-arrays to strip out the ID of eacch course a friend has liked and push those into a new array.
     
     friends_courses = Array.new
     friends_courses_ids = Array.new
@@ -40,22 +47,8 @@ class ResultsController < ApplicationController
       friends_courses.push(User.where({ :id => an_id }).at(0).courses)
       friends_courses.each do |a_course|
         friends_courses_ids.push(a_course.at(0).id)
-      end
     end
-
-@matching_course_ids = Array.new
-    recipient_ids.each do |a_recipient|
-      @matching_course_ids << User.where({ :id => a_recipient }).at(0).likes.pluck(:course_id)
     end
-
-# matching_course_ids
-matching_hash = @matching_course_ids.flatten.group_by{|e| e}.map{|k, v| [k, v.length]}.to_h
-
-# delete this instance variable if i don't end up using it
-@matching_hash = @matching_course_ids.flatten.group_by{|e| e}.map{|k, v| [k, v.length]}.to_h
-
-
-    render({ :template => "results/index.html.erb" })
 
   end
   
