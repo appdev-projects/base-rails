b7c2264c88d3d1aac058cb415f6ea9a65618fb46
wfswee <wfswee@chicagobooth.edu>
2020-11-21 17:05:47 +0000

    after deleting more results controller code, before trying to reduce the code

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,6 +1,17 @@
 class ResultsController < ApplicationController
   def index
 
+    # WORKFLOW:
+    #  1. Who are the current user's friends?
+    #  2. What courses do those friends like? I can't write an associate to get this because those tables aren't linked.
+    #  3. For each course the current user has liked, count the number of their friends who have liked each of those courses. Be careful not to include the current user when calculating the number of likes.
+    #. 4. Store the course names and number of likes into an object, then print the results into a table.
+
+    # LET'S BEGIN
+
+    # 1. FIND THE CURRENT USER'S FRIENDS
+
+    # (a) Create a blank array and push any friend record into it where the current user's id is the sender id.
     users_friends = Array.new
 
     all_friends = Friend.all
@@ -12,28 +23,28 @@ class ResultsController < ApplicationController
       end
     end
 
-    # Extract the recipient_id from each of those friends so I can match those users with the user table
+    # (b) Extract the recipient id from each of those friend records and push them into a new array.
 
-    @recipient_ids = Array.new
+    recipient_ids = Array.new
 
     users_friends.each do |a_friend|
-      @recipient_ids.push(a_friend.recipient_id)
+      recipient_ids.push(a_friend.recipient_id)
     end
 
-
+    # (c) 
     
-    @friends_courses = Array.new
-    @friends_courses_ids = Array.new
+    friends_courses = Array.new
+    friends_courses_ids = Array.new
 
-    @recipient_ids.each do |an_id|
-      @friends_courses.push(User.where({ :id => an_id }).at(0).courses)
-      @friends_courses.each do |a_course|
-        @friends_courses_ids.push(a_course.at(0).id)
+    recipient_ids.each do |an_id|
+      friends_courses.push(User.where({ :id => an_id }).at(0).courses)
+      friends_courses.each do |a_course|
+        friends_courses_ids.push(a_course.at(0).id)
       end
     end
 
 @matching_course_ids = Array.new
-    @recipient_ids.each do |a_recipient|
+    recipient_ids.each do |a_recipient|
       @matching_course_ids << User.where({ :id => a_recipient }).at(0).likes.pluck(:course_id)
     end
 
@@ -42,14 +53,6 @@ matching_hash = @matching_course_ids.flatten.group_by{|e| e}.map{|k, v| [k, v.le
 
 # delete this instance variable if i don't end up using it
 @matching_hash = @matching_course_ids.flatten.group_by{|e| e}.map{|k, v| [k, v.length]}.to_h
-
-
-# fetching hash
-@match_counts = matching_hash.map { |k,v| "#{Course.where({ :id => k }).at(0).name}} is liked by #{v} of your friends!" }
-
-
-@match_counts_k = matching_hash.map { |k,v| "#{Course.where({ :id => k }).at(0).name}" }
-@match_counts_v = matching_hash.map { |k,v| "#{v}" }
 
 
     render({ :template => "results/index.html.erb" })
