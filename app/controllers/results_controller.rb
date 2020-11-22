b70d9f5ef0e92daa8c4f1d9f18900ba06bdada92
wfswee <wfswee@chicagobooth.edu>
2020-11-22 22:32:54 +0000

    before starting on sample data rake

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,13 +1,8 @@
 class ResultsController < ApplicationController
   def index
 
-    # WORKFLOW:
-    #  1. Who are the current user's friends?
-    #  2. What courses do those friends like? I can't write an associate to get this because those tables aren't linked.
-    #  3. For each course the current user has liked, count the number of their friends who have liked each of those courses. Be careful not to include the current user when calculating the number of likes.
-    #  4. Store the course names and number of likes into an object, then print the results into a table.
+    # (1) Create a blank array and push all the IDs of the current user's friends into it.
 
-    # (a) Create a blank array and push all the IDs of the current user's friends into it.
     recipient_ids = Array.new
 
     all_friends = Friend.all
@@ -19,31 +14,40 @@ class ResultsController < ApplicationController
       end
     end
 
-    # (b) For each friend, pluck out the IDs of all the courses they have liked and push it into a new array.
+    # (2) For each friend, pluck out the IDs of all the courses they like and push them into a new array.
     
     @matching_course_ids = Array.new
       recipient_ids.each do |a_recipient|
         @matching_course_ids << User.where({ :id => a_recipient }).at(0).likes.pluck(:course_id)
       end
     
-    # (d) Getting back to counting how many of the current user's friends liked each of these courses, flatten the array and store in a hash key value pairs where key = course id and value = instances of that course in the friends' likes.
+    # (3) Flatten the array, group by course, and store the result in a hash where key = course id and value = likes that course id received (for purposes of later counting how many there are stored in each value).
 
     @matching_hash = @matching_course_ids.flatten.group_by{|e| e}.map{|k, v| [k, v]}.to_h
 
-        # (c) But I need to filter out those courses that the current user hasn't liked, so I need to get current user course ID's.
+    # (4) But I need to filter out those courses that the current user hasn't liked, so I need to pluck out current user liked course ID's.
 
     @current_user_course_ids = Array.new
     @current_user_course_ids << User.where({ :id => @current_user.id }).at(0).likes.pluck(:course_id)
     @current_user_course_ids = @current_user_course_ids.flatten
 
-    # (d) And so here's what I'm after. An array of the course IDs that the current user is intersted in and that at least one of their friends is intersted in.
+    # (5) And then I can use that to filter so eliminate courses that the current user hasn't liked.
     
     @overlapping_course_ids = @matching_course_ids.flatten & @current_user_course_ids
 
-    # (d) Render the page that will show the resultst.
+    # (6) Now I convert each key and value into the information I actually want to show the user, and I push them into a new has that will make it easy to print this information across two columns in a table.
+
+    @printable_results = Hash.new
+    @matching_hash.each do |k, v|
+    if @overlapping_course_ids.include? k
+    @printable_results.store(Course.where({ :id => k }).at(0).name,v.length)
+    end
+    end
+
+
+    # (7) There we go. Now render the page that will show the resultst.
 
     render({ :template => "results/index.html.erb" })
-
+    
   end
-  
 end
--- lib/tasks/dev.rake
+++ lib/tasks/dev.rake
@@ -1,3 +1,23 @@
 desc "Hydrate the database with some sample data to look at so that developing is easier"
 task({ :sample_data => :environment}) do
+
+
+  User.detroy_all
+Flight.destroy_all
+
+u = User.new
+u.email = "here"
+u.password = "here"
+u.save
+
+p u.errors.full_messages
+
+first_flight = Flight.new
+first_flight.description = "here"
+first_flight.departs_at = 24.hours.from_new + 5.min
+first_flight.user_id = u.id
+first_flight.save
+
+p first_flight.errors.full_messages
+
 end
