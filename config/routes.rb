2cc025374a483ea38f782772d6a2bbb9401ad725
jhoffmeyer <jhoffmeyer@chicagobooth.edu>
2021-06-04 05:35:22 +0000

    minor stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  root "arcades#index"
+
+  devise_for :users
+
   resources :machines
   resources :favorites
   resources :comments
-  root "arcades#index"
-
-
   resources :arcades
   resources :games
-  devise_for :users
+  
 end
